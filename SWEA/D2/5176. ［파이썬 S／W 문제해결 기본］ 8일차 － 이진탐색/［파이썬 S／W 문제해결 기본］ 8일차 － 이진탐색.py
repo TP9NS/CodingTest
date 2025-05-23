48b9787cebed75e2c5a1b5fbc87cf2c8994aa403
TP9NS <118120735+TP9NS@users.noreply.github.com>
2025-05-23 14:27:00 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 8일차 - 이진탐색, Time: 76 ms, Memory: 58,624 KB -BaekjoonHub

--- SWEA/D2/5176. ［파이썬 S／W 문제해결 기본］ 8일차 － 이진탐색/［파이썬 S／W 문제해결 기본］ 8일차 － 이진탐색.py
+++ SWEA/D2/5176. ［파이썬 S／W 문제해결 기본］ 8일차 － 이진탐색/［파이썬 S／W 문제해결 기본］ 8일차 － 이진탐색.py
@@ -1,18 +1,15 @@
-def in_order_fill(node, N, tree, values, index):
-    if node > N:
-        return index
-    index = in_order_fill(node * 2, N, tree, values, index)
-    tree[node] = values[index]
-    index += 1
-    index = in_order_fill(node * 2 + 1, N, tree, values, index)
-    return index
+def inorder(idx):  # idx: 현재 노드 번호
+    global cnt
+    if idx <= N:
+        inorder(idx * 2)         # 왼쪽 자식 방문
+        tree[idx] = cnt          # 현재 노드에 cnt값 저장
+        cnt += 1
+        inorder(idx * 2 + 1)     # 오른쪽 자식 방문
 
 T = int(input())
-for test_case in range(1, T + 1):
-    N = int(input())
-    tree = [0] * (N + 1)  # 1번부터 N번까지 사용
-    values = list(range(1, N + 1))  # 1부터 N까지 숫자
-    
-    in_order_fill(1, N, tree, values, 0)
-
-    print(f"#{test_case} {tree[1]} {tree[N // 2]}")
+for tc in range(1, T + 1):
+    N = int(input())             # 트리의 총 노드 수
+    tree = [0] * (N + 1)         # 1번부터 쓰기 위해 N+1 크기 배열
+    cnt = 1                      # 1부터 시작해서 순서대로 채움
+    inorder(1)                   # 루트(1번 노드)부터 순회 시작
+    print(f"#{tc} {tree[1]} {tree[N // 2]}")
