966d33afbd0a856c276c7d5d762083ee4ac5469a
TP9NS <118120735+TP9NS@users.noreply.github.com>
2025-12-20 20:41:05 +0900

    [level 2] Title: 프로세스, Time: 6.49 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,21 +1,22 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
-    dq= deque()
+    dq = deque()
+    answer= 0
+    
     for i,j in enumerate(priorities):
         dq.append((i,j))
-    
-    max_pri = max(dq, key= lambda x : x[1])[1]
-    
-    while True:
-        idx,pri = dq.popleft()
-        if max_pri == pri:
+        
+    while dq:
+        max_dq = max(dq,key=lambda x : x[1])[1]
+        temp = dq.popleft()
+        if temp[1] == max_dq:
             answer+=1
-            if idx == location:
-                break
+            if temp[0] == location:
+                return answer
             else:
-                max_pri = max(dq, key= lambda x : x[1])[1]
-        else :
-            dq.append((idx,pri))
+                max_dq = max(dq,key=lambda x: x[1])[1]
+        else:
+            dq.append(temp)
+                
     return answer
